2ca1da717f14aae37dbf257551248d3060b90d94
Simon Winther <simonwinther22@gmail.com>
2020-11-16 14:37:14 +0100

    Added jobscript

---
+++ jobscript.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+#BSUB -q qpuv100
+#BSUB -gpu "num=1"
+#BSUB -J myJob
+#BSUB -n 1
+#BSUB -W 10:00
+#BSUB -R "rusage[mem=32GB]"
+#BSUB -i logs/%J.out
+#BSUB -e logs/%J.err
+
+echo "Running script"
+python3 training.py
