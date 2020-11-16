d8c7251711511d041553878d9c59045afbb56845
Simon Winther <simonwinther22@gmail.com>
2020-11-16 14:49:29 +0100

    Added hyperparams

--- training.py
+++ training.py
@@ -4,6 +4,18 @@ import torch.nn.functional as F
 from utils import make_env, Storage, orthogonal_init
 from model import Flatten, Encoder, Policy
 
+# Hyperparameters
+total_steps = 8e6
+num_envs = 32
+num_levels = 10
+num_steps = 256
+num_epochs = 3
+batch_size = 512
+eps = .2
+grad_eps = .5
+value_coef = .5
+entropy_coef = .01
+
 # Define environment
 # check the utils.py file for info on arguments
 env = make_env(num_envs, num_levels=num_levels)
