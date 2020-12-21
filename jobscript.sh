31df1030b321a6a13ec8424cf7454c52a2bc6a9d
Stephane Jean Bruno Guichard <stephane.gcrd@gmail.com>
2020-12-21 09:20:06 +0100

    new script

--- jobscript.sh
+++ jobscript.sh
@@ -3,10 +3,13 @@
 #BSUB -gpu "num=1"
 #BSUB -J 50_levels
 #BSUB -n 1
-#BSUB -W 3:00
+#BSUB -W 6:00
 #BSUB -R "rusage[mem=32GB]"
 #BSUB -o %J.out
 #BSUB -e %J.err
+
+N_LEVEL=50
+N_STEPS=5e6
 
 module load python3/3.8.0
 module load cuda/8.0
@@ -19,5 +22,10 @@ unset PYTHONPATH
 
 cd ~/DeepLearning/DeepLearningProject
 
+RUN_NAME=$N_LEVEL
+RUN_NAME+=_levels_hard
+
+echo $RUN_NAME
+
 echo "Running script"
-python3 training.py --run_name 50_levels_BNR --total_steps 4e6 --num_levels 50 --num_envs 32 --value_coef 0.5 --distribution_mode hard
+python3 training.py --run_name $RUN_NAME --total_steps $N_STEPS --num_levels $N_LEVEL --num_envs 32 --value_coef 0.5 --distribution_mode hard
